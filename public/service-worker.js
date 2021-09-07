b574283a0fee018d0eec26a3e611f4b13eea07ee
jerismith32 <jerismith32@gmail.com>
2021-09-07 13:29:10 -0400

    updated offline functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,16 @@ const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "index.html",
   "js/index.js",
-  "css/style.css"
+  "css/style.css",
+  "manifest.json",
+  "icons/icon-72x72.png",
+  "icons/icon-512x512.png",
+  "icons/icon-384x384.png",
+  "icons/icon-192x192.png",
+  "icons/icon-152x152.png",
+  "icons/icon-144x144.png",
+  "icons/icon-128x128.png",
+  "icons/icon-96x96.png",
 ];
 
 // Respond with cached resources
@@ -34,7 +43,7 @@ self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
+    //   return cache.addAll(FILES_TO_CACHE)
     })
   )
 })
