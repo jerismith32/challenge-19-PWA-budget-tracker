85b0be35d8d68f3cd128953a0b5ee670a74fcc1b
jerismith32 <jerismith32@gmail.com>
2021-09-09 19:12:22 -0400

    updated for connection

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-//const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-const uri = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const uri = process.env.MONGODB_URI;
 const app = express();
 
 app.use(logger("dev"));
